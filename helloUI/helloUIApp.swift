16cad0b6194f341292d3dc8bf83d8a0ff153a252
Erik Gomez <e@eriknicolasgomez.com>
2021-06-08 20:07:41 -0500

    Initial Commit

---
+++ helloUI/helloUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloUIApp.swift
+//  helloUI
+//
+//  Created by Erik Gomez on 6/8/21.
+//
+
+import SwiftUI
+
+@main
+struct helloUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
