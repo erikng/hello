55b1091da97c422f7c1a863c452b21cd9c015b38
Erik Gomez <e@eriknicolasgomez.com>
2021-06-10 20:04:18 -0500

    stupid temporary hack to get list to refresh

--- hello/UI/Primary/Status.swift
+++ hello/UI/Primary/Status.swift
@@ -9,51 +9,64 @@ import SwiftUI
 
 //TODO: Hook this into the JSON preferences file
 
+let helloRefreshCycleTimer = Timer.publish(every: Double(1), on: .main, in: .common).autoconnect()
+
 // Status
 struct Status: View {
+    @State var refreshUI = false
     var body: some View {
-        List(installStages1) { stage in
-            VStack(alignment: .leading) {
-                StageRow(installstage: stage)
+        VStack {
+            List(installStages1) { stage in
+                VStack(alignment: .leading) {
+                    StageRow(installstage: stage)
+                    Rectangle()
+                        .fill(Color.gray.opacity(0.5))
+                        .frame(height: 1)
+                }
+            }
+            .cornerRadius(10)
+            .frame(width: 876, height: 330)
+        
+            // Divider
+            HStack {
                 Rectangle()
                     .fill(Color.gray.opacity(0.5))
                     .frame(height: 1)
             }
+            .frame(width: 900)
             
+            // TODO: Learn Swift
+            // This is fucking stupid
+            Text(String(self.refreshUI))
+                .hidden()
+                .frame(width: 0, height: 0)
+                .onReceive(helloRefreshCycleTimer) { _ in
+                    self.refreshUI.toggle()
+                }
+            
+            // Secondary Status
+            // TODO: Figure out if this object can be pushed down about 20 pixels so it matches the top part
+            HStack {
+                Image(nsImage: Utils().createImageData(fileImagePath: "/Applications/Slack.app/Contents/Resources/electron.icns"))
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .scaledToFit()
+                    .frame(width: 30, height: 30)
+                    .padding(.leading, 15)
+                Text("Slack")
+                    .fontWeight(.bold)
+                Text("is installing")
+                    .fontWeight(.light)
+                    .padding(.leading, -5)
+                Spacer()
+            }
+            .frame(width: 876)
         }
-        .cornerRadius(10)
-        .frame(width: 876, height: 330)
-    
-        // Divider
-        HStack {
-            Rectangle()
-                .fill(Color.gray.opacity(0.5))
-                .frame(height: 1)
-        }
-        .frame(width: 900)
-        
-        // Secondary Status
-        // TODO: Figure out if this object can be pushed down about 20 pixels so it matches the top part
-        HStack {
-            Image(nsImage: Utils().createImageData(fileImagePath: "/Applications/Slack.app/Contents/Resources/electron.icns"))
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .scaledToFit()
-                .frame(width: 30, height: 30)
-                .padding(.leading, 15)
-            Text("Slack")
-                .fontWeight(.bold)
-            Text("is installing")
-                .fontWeight(.light)
-                .padding(.leading, -5)
-            Spacer()
-        }
-        .frame(width: 876)
     }
 }
 
 struct InstallStage: Codable, Identifiable {
-    var id: Int
+    var id = UUID()
     let title: String
     let iconPath: String
     let installedPath: String
@@ -63,6 +76,7 @@ struct InstallStage: Codable, Identifiable {
 // Stage Status (Dynamic Row)
 struct StageRow: View {
     var installstage: InstallStage
+    @State var refreshUI = false
     var body: some View {
         HStack {
             // Icon
@@ -83,6 +97,15 @@ struct StageRow: View {
                     .scaledToFit()
                     .frame(width: 40, height: 40)
             }
+            // TODO: Learn Swift
+            // This is fucking stupid
+            Text(String(self.refreshUI))
+                .hidden()
+                .frame(width: 0, height: 0)
+                .onReceive(helloRefreshCycleTimer) { _ in
+                    self.refreshUI.toggle()
+                }
+
             // Stage Name
             Text("\(installstage.title)")
                 .font(.body)
@@ -117,25 +140,21 @@ let installStages = deviceStages
 
 let installStages1 = [
     InstallStage(
-        id: 1,
         title: "Xcode Command Line Tools",
         iconPath: "/System/Applications/Utilities/Terminal.app/Contents/Resources/Terminal.icns",
         installedPath: "/Applications/Xcode.app"
     ),
     InstallStage(
-        id: 2,
         title: "Visual Studio Code",
         iconPath: "/Applications/Visual%20Studio%20Code.app/Contents/Resources/Code.icns",
         installedPath: "/Applications/Visual Studio Code.app"
     ),
     InstallStage(
-        id: 3,
         title: "Slack",
         iconPath: "/Applications/Slack.app/Contents/Resources/electron.icns",
         installedPath: "/Applications/Slack.app"
     ),
     InstallStage(
-        id: 4,
         title: "GitHub Desktop",
         iconPath: "/Applications/GitHub%20Desktop.app/Contents/Resources/electron.icns",
         installedPath: "/Applications/GitHub Desktop.app"
