daea30100c0a833b17a59a4360ab58ec062fc024
Erik Gomez <e@eriknicolasgomez.com>
2021-06-11 10:26:29 -0500

    create base exit view

---
+++ hello/UI/ExitView.swift
@@ -0,0 +1,20 @@
+//
+//  ExitView.swift
+//  hello
+//
+//  Created by Erik Gomez on 6/11/21.
+//
+
+import SwiftUI
+
+struct ExitView: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+struct ExitView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExitView()
+    }
+}
